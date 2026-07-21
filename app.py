9a85c07a03b9f7783ccc150790020fe4f14b9766
sillfrog999 <nguoiandanhthichchoiff@gmail.com>
2026-07-21 22:04:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -149,13 +149,12 @@ def dashboard():
 
 
 
+with app.app_context():
+
+	db.create_all()
+
+
 if __name__=="__main__":
-
-
-	with app.app_context():
-
-		db.create_all()
-
 
 	app.run(
 		host="0.0.0.0",
