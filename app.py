260780e83e8bbbc6558615ada70e8c8f24d349d1
sillfrog999 <nguoiandanhthichchoiff@gmail.com>
2026-07-22 23:55:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,17 @@ app=Flask(__name__)
 
 app.config["SECRET_KEY"]="bines"
 
-app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///bines.db"
+database_url=os.environ.get("DATABASE_URL")
+
+if database_url:
+	database_url=database_url.replace(
+		"postgres://",
+		"postgresql://",
+		1
+	)
+
+app.config["SQLALCHEMY_DATABASE_URI"]=database_url
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
 
 
 
